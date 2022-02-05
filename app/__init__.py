769cc1e3a9440e83d718fb1fbda78807fcb0878e
vernchao91 <vernchao91@gmail.com>
2022-02-04 19:59:02 -0800

    structure for python

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,5 @@ from app.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# print(app.config['SECRET_KEY'])
 
-@app.route('/')
-def index():
-  return render_template("index.html", var="Python test")
+from app import routes
