e88db72483176ec76ae66d283d5b0aacf16e7eed
vernchao91 <vernchao91@gmail.com>
2022-02-19 22:18:49 -0800

    modified routes

--- app/routes.py
+++ app/routes.py
@@ -3,3 +3,11 @@ from app import app
 @app.route('/')
 def index():
   return render_template("index.html", var="Python test")
+
+@app.route('/help')
+def help():
+  return "<h1>help</h1>"
+
+@app.route('/item/<id>')
+def item(id):
+  
